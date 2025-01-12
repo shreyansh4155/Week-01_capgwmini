691516416939356847e49f9a85d160f6c263258d
unknown <shreyanshg245@gmail.com>
2025-01-12 17:16:32 +0530

    Program - 5

---
+++ Level-01/HarryAge.java
@@ -0,0 +1,14 @@
+class SamAverage {
+    public static void main(String[] args) {
+       //initializing marks
+
+       int sMath = 94;
+       int sPhysics = 95;
+       int sChemistry = 96;
+       
+       //calculating average of the subject
+       
+       double avg = (sMath + sPhysics + sChemistry)/3;
+       System.out.println("Sam average mark in PCM is " + avg);
+    }
+}
