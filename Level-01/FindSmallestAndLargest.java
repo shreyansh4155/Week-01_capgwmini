8f2f02b4865850875f5e8d721936408e69720cdc
unknown <shreyanshg245@gmail.com>
2025-01-12 18:04:36 +0530

    Day-04 Level-01 program 02 added

---
+++ Level-01/FindSmallestAndLargest.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class FindSmallestAndLargest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int[] result = findSmallestAndLargest(num1, num2, num3);
+
+        System.out.println("Smallest number: " + result[0]);
+        System.out.println("Largest number: " + result[1]);
+
+        scanner.close();
+    }
+
+    public static int[] findSmallestAndLargest(int number1, int number2, int number3) {
+        int smallest, largest;
+
+        // Finding the smallest number
+        if (number1 <= number2 && number1 <= number3) {
+            smallest = number1;
+        } else if (number2 <= number1 && number2 <= number3) {
+            smallest = number2;
+        } else {
+            smallest = number3;
+        }
+
+        // Finding the largest number
+        if (number1 >= number2 && number1 >= number3) {
+            largest = number1;
+        } else if (number2 >= number1 && number2 >= number3) {
+            largest = number2;
+        } else {
+            largest = number3;
+        }
+
+        int[] result = {smallest, largest};
+        return result;
+    }
+}
