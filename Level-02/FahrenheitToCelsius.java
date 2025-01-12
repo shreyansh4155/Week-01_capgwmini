c9f74aaa69b12501a35879560073330f3ba93760
unknown <shreyanshg245@gmail.com>
2025-01-12 17:23:35 +0530

    Level-02 program 3 added

---
+++ Level-02/FahrenheitToCelsius.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = scanner.nextDouble();
+
+        // Convert Fahrenheit to Celsius
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println(fahrenheit + " Fahrenheit is " + celsius + " Celsius");
+
+        scanner.close();
+    }
+}
+
